b8481435288b90d4f133575c1d53319c19f54fb5
João Passareli <joaopassareli@gmail.com>
2023-07-18 21:15:39 -0300

    commit inicial do projeto

---
+++ src/Controller/VideoListController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alura\Mvc\Controller;
+
+use Alura\Mvc\Repository\VideoRepository;
+
+class VideoListController extends ControllerWithHtml implements Controller
+{
+    public function __construct(private VideoRepository $videoRepository)
+    {
+    }
+
+    public function processaRequisicao(): void
+    {
+        $videoList = $this->videoRepository->all();
+        echo $this->renderTemplate(
+            'video-list',
+            ['videoList' => $videoList]
+        );
+    }
+}
